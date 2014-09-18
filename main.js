0514feccd473fc9ca88a3b4ea503b1fefa07409b
Roy McFarland <roy.mcfarland@gmail.com>
2014-09-18 17:06:32 -0600

    started part 2

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
 $(document).on('ready', function() {
 	
-	$(document).on('click', '.input', function() {
-		console.log('Click handler fired');
-		$(this).after(
-			'<textarea class="new-text-box">'+
-			'Insert Text'+
-			'</textarea>'
-			)
-		$(this).remove();
-		
+
+	/////////////////////////////////
+	///////////  PART 1  ////////////
+	/////////////////////////////////
+
+	// $(document).on('click', '.input', function() {
+	// 	console.log('Click handler fired')
+	// 	$(this).after(
+	// 		'<textarea class="new-text-box">'+
+	// 		'Insert Text'+
+	// 		'</textarea>'
+	// 		)
+	// 	$(this).hide()
+	// })
+
+
+
+	/////////////////////////////////
+	///////////  PART 2  ////////////
+	/////////////////////////////////
+
+	$(document).on('click', '.text-box', function() {
+		var text = $('.text-box').text()
+		console.log('Click handler fired')
+		$(this).after('<textarea class="new-text-box"></textarea>')
+		$(this).hide()
+		$('.new-text-box').val(text)
 	})
   
 });
